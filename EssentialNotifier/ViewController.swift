0faf02c98131302771f812763fecacec73e85b19
Gabriel T <gabriel.tapuc@gmail.com>
2017-09-27 14:07:46 -0400

    A notification now gets scheduled.

--- EssentialNotifier/ViewController.swift
+++ EssentialNotifier/ViewController.swift
@@ -16,9 +16,45 @@ class ViewController: UIViewController {
         
         // 1. Request permission
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (granted, error) in
-            
+            if granted {
+                print("Notification access granted")
+            } else {
+                print(error!.localizedDescription)
+            }
         })
     }
+    
+    @IBAction func notifyMeButtonTapped(_ sender: Any) {
+        scheduleNotification(inSeconds: 5, completion: { success in
+            if success {
+                print("Successfully scheduled notification")
+            } else {
+                print("Error scheduling notification")
+            }
+        })
+    }
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        let notif = UNMutableNotificationContent()
+        
+        notif.title = "New Notification"
+        notif.subtitle = "These are great!"
+        notif.body = "The new notification options in iOS 10 are great!"
+        
+        let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notificationTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error!)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        })
+    }
+    
 
 }
 
